cacd0b3b3d4a944f9d03deec9c54a4e2599a6262
Ansh Bisht <ansh.bisht@example.com>
2026-05-27 11:12:09 +0530

    fix: update ALLOWED_HOSTS to allow any host connection in production Render settings

--- flightproject/backend/config/settings.py
+++ flightproject/backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1^g-kx8@63=008f35zp4epn-zqgm=b9_g-t&0v%d(9lr6623vj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
